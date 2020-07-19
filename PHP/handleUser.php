6abd2e34c19b89475e5fe05303212c6778c46e1a
Salipa-Gurung <salipagurung@gmail.com>
2020-07-19 04:52:50 -0700

    added handling signup process

---
+++ PHP/handleUser.php
@@ -0,0 +1,29 @@
+<?php
+
+if(isset($_POST['signup'])){
+
+
+    $email = $_POST['Email'];
+    $username =  $_POST['Uname'];
+    $password =  $_POST['Password'];
+    $confirmpassword =  $_POST['Repass'];
+    $phonenumber =  $_POST['Phn'];
+
+    if($password == $confirmpassword){
+        require_once "./connection.php";
+
+        $sql = "INSERT INTO users(USERNAME, EMAIL, PASSWORD, PHN) VALUES('$username', '$email', '$password', '$phonenumber')";
+
+        mysqli_query($connect, $sql);
+
+        if(mysqli_affected_rows($connect)){
+            header("location: ../PAGES/profile.php?Logged");
+        }else{
+            header("location: ../Landing.php");
+        }
+    }
+}else{
+    header("location:../Landing.php");
+}
+
+?>
