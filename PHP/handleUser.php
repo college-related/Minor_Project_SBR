5e58d8771b87f4dfbfcc5ee6cdded57d95fc43b1
Alson33 <be2018se607@gces.edu.np>
2020-07-23 18:31:56 +0545

    checking already user security added

--- PHP/handleUser.php
+++ PHP/handleUser.php
@@ -12,18 +12,29 @@ if(isset($_POST['signup'])){
     if($password == $confirmpassword){
         require_once "./connection.php";
 
-        session_start();
-        $_SESSION['Uname'] = $username;
+        $sql_email_check = "SELECT * FROM users WHERE EMAIL = '$email';";
+        $sql_phn_check = "SELECT * FROM users WHERE PHN = '$phonenumber';";
 
-        $sql = "INSERT INTO users(USERNAME, EMAIL, PASSWORD, PHN) VALUES('$username', '$email', '$password', '$phonenumber')";
+            $query_email_check = mysqli_query($connect, $sql_email_check);
+            $query_phn_check = mysqli_query($connect, $sql_phn_check);
 
-        mysqli_query($connect, $sql);
-
-        if(mysqli_affected_rows($connect)){
-            header("location: ../PAGES/profile.php?Logged");
+        if(mysqli_num_rows($query_email_check) > 0 || mysqli_num_rows($query_phn_check) > 0){
+            header("location: ../Landing.php#signupForm");
         }else{
-            header("location: ../Landing.php");
+            session_start();
+            $_SESSION['Uname'] = $username;
+
+            $sql = "INSERT INTO users(USERNAME, EMAIL, PASSWORD, PHN) VALUES('$username', '$email', '$password', '$phonenumber')";
+
+            mysqli_query($connect, $sql);
+
+            if(mysqli_affected_rows($connect)){
+                header("location: ../PAGES/profile.php?Logged");
+            }else{
+                header("location: ../Landing.php");
+            }
         }
+
     }else 
     {
         header("location:../Landing.php");
