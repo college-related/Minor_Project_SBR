e40e2a0bad8c23f1a8f5a202e81719b858d9818e
Alson33 <be2018se607@gces.edu.np>
2020-07-23 17:18:15 +0545

    dynamic list from js

---
+++ JS/category_type.js
@@ -0,0 +1,23 @@
+
+function changingType(sel1, sel2){
+    var select1 = document.getElementById(sel1);
+    var select2 = document.getElementById(sel2);
+    
+    select2.innerHTML = "";
+
+    var optArry = [];
+
+    if(select1.value == "twoWheel"){
+        optArry = ["k|K", "a|A"];
+    }else{
+        optArry = ["b|B", "c|C", "d|D", "e|E", "f|F", "g|G", "h|H", "i|I", "j|J"];
+    }
+
+    for(var option in optArry){
+        var pair = optArry[option].split("|");
+        var newOption = document.createElement("option");
+        newOption.value = pair[0];
+        newOption.innerHTML = pair[1];
+        select2.options.add(newOption);
+    }
+}
