78eb1766963281e0e6cdd2af1e494f4b48b419f9
Alson33 <garbuja12alson@gmail.com>
2020-09-09 09:53:20 +0545

    new design implementation

--- Landing.php
+++ Landing.php
@@ -116,21 +116,21 @@
 
         <!-- navigation bar for desktop view -->
         <nav class="web-navigation">
-            <a href="#">Home</a>
-            <a href="./PAGES/information.php?isLogged=false">Infos</a>
-            <a href="#signupForm">SignUp</a>
-            <!-- <a href="#loginForm">LogIn</a> -->
+            <a href="./pages/information.php?isLogged=false">Infos</a>
+            <a href="#signupForm" onclick="signPopUp()">SignUp</a>
+            <a href="#loginForm" onclick="logPopUp()">LogIn</a>
         </nav>
+
         <!-- navigation bar for mobile view -->
         <nav class="mobile-navigation">
             <ul>
-                <li>Home</li>
                 <li>Infos</li>  
                 <li>SignUp</li>
                 <li>LogIn</li>
             </ul>
         </nav>
     </header>
+
     <div class="nav-hero">
              <!-- hero text(website name) -->
         <h1 class="hero-text headings">
@@ -144,7 +144,7 @@
 
         <!-- getting started button -->
         <a href="#signupForm" class="hero-link">
-            <button id="hero-btn">GET STARTED</button>
+            <button id="hero-btn" onclick="signPopUp()">GET STARTED</button>
         </a>
         </div>
        
@@ -249,9 +249,6 @@
 
     </main>
 
-    <!-- wave structure above footer-->
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#7293b5" fill-opacity="1" d="M0,192L48,197.3C96,203,192,213,288,202.7C384,192,480,160,576,133.3C672,107,768,85,864,96C960,107,1056,149,1152,144C1248,139,1344,85,1392,58.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
-    
     <!-- review section -->
     <div class="review-section">
 
