b4aa6f75324c783d642f1c5c9c40972279998d95
Salipa-Gurung <salipagurung@gmail.com>
2020-09-12 00:23:44 -0700

    new profile page

--- PAGES/profile.php
+++ PAGES/profile.php
@@ -7,7 +7,18 @@
     require "../PHP/connection.php";
 
     session_start();
+
+    $ph = $_SESSION['phone'];
+
+    $sql_form = "SELECT * FROM form WHERE PHN = '$ph';";
+    $form_result = mysqli_query($connect,$sql_form);
+    $sql_basic = "SELECT * FROM basic_data WHERE PHN = '$ph';";
+    $basic_result = mysqli_query($connect,$sql_basic);
+
+    $formArray = mysqli_fetch_all($form_result,MYSQLI_ASSOC);
+    $basicArray = mysqli_fetch_all($basic_result,MYSQLI_ASSOC);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,6 +36,48 @@
     <link href="https://fonts.googleapis.com/css2?family=Mukta+Malar&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet">
 
+    
+    <style>
+        #closeBtn-holder{
+            display:<?php
+            if(isset($_GET['signed'])){
+                echo "none;";
+            }
+            else{
+                echo "block;";
+            }
+            ?>
+            text-align:right;
+        }
+        .information{
+            display:
+            <?php
+            if(isset($_GET['signed'])){
+                echo "block;";
+            }
+            else{
+                echo "none;";
+            }
+            ?>
+        }
+        #close-mark{
+            text-align:right;
+            font-size:40px;
+            color:white;
+        }
+        #close-mark,.fa-edit{
+            cursor:pointer;
+        }
+        <?php
+        if(isset($_GET['signed'])){
+            echo ".detail-popup, .bg-overlay{
+                display:block;
+                 }";
+        }
+        ?>
+
+    </style>
+    <script defer src="../JS/detail_popup.js"></script>
     <script defer src="../JS/category_type.js"></script>
 </head>
 <body>
@@ -51,20 +104,153 @@
        
     </header>
 
-    <!-- <div class="nav-hero"> -->
-        <!-- hero text(website name) -->
-        <!-- <h1 class="hero-text headings">
+    <div class="nav-hero">
+        <h1 class="hero-text headings">
             PROFILE
         </h1>
-    </div> -->
-
+    </div>
     <!-- wave sturcture below header-->
     <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#7293b5" fill-opacity="1" d="M0,224L48,197.3C96,171,192,117,288,96C384,75,480,85,576,101.3C672,117,768,139,864,160C960,181,1056,203,1152,202.7C1248,203,1344,181,1392,170.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg> -->
-
+   
+   <div class="bg-overlay"></div>
+   
+    <div class="detail-popup">
+        <div class="information"><P>Fill Up the data please</P></div>
+        <div id="closeBtn-holder">
+            <span id="close-mark" onclick="closedEditPopup()">
+            &times;
+            </span>
+    </div>
+    <form action="../PHP/handleBasicdata.php" method="POST">
+                <table>
+                    <tr>
+                        <td>
+                            <label for="vehicle-type">Vehicle Type</label>
+                        </td>
+                        <td>
+                        <select name="Vtype" id="vehicle-type" onchange="changingType('vehicle-type', 'vehicle-name')" required>
+                                <option value="none"></option>
+                                <option value="twoWheel">Two wheeler</option>
+                                <option value="fourWheel">Four wheeler</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="vehicle-name">Vehicle Category</label>
+                        </td>
+                        <td><select name="Vcategory" id="vehicle-name" required></select></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="engine-cc">Engine cc</label>
+                        </td>
+                        <td><input type="number" name="ECC" id="engine-cc" placeholder="Ex: 150"></td>
+                    </tr>
+                   
+                    <tr>
+                        <td>
+                            <label for="registration-no">Vehicle Registration Number </label>
+                        </td>
+                        <td><input type="text" name="regNo" id="registration-no" placeholder="GA 16 PA 4381"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="renewDate">Last Renew Date:</label></td>
+                        <td><input type="date" name="RenewDate" id="renewDate" /></td>
+                    </tr>
+                    <tr>
+                        <td > 
+                      </td>
+                        <td class="btn-td" colspan=2>
+                            <input type="submit" name="savebtn" class="editsavebtn" value="SAVE">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+    </div>
     <main>
-        <!-- TODO -->
-    </main>
- 
+        <div id="profile" class="maindiv">
+            <div id="add-photo">
+                <a href="#" id="profile-photo" style="color: black;">Add Photo</a>
+            </div>
+            <div id="user-name">
+                <h4>
+                    <?php
+                        echo $_SESSION['Uname'];
+                    ?>
+                </h4>
+            </div>
+        </div>
+
+        <div id="details" class="maindiv">
+            <h3><u> DETAILS</u></h3>
+            <div id="detail-form">
+                <?php
+                foreach($basicArray as $data){
+                ?>
+                <table>
+                    <tr>
+                        <td>Phone number :</td>
+                        <td> <?=$data['PHN']?> </td>
+                    </tr>
+                    <tr>
+                        <td>Vehicle Type:</td>
+                        <td> <?=$data['VEHICLE_TYPE']?></td>
+                    </tr>
+                    <tr>
+                        <td>Vehicle Category :</td>
+                        <td> <?=$data['VEHICLE_CAT']?></td>
+                    </tr>
+                    <tr>
+                        <td>ENGINE_CC :</td>
+                        <td> <?=$data['ENGINE_CC']?></td>
+                    </tr>
+                    <tr>
+                        <td>Vehicle Registration no. :</td>
+                        <td> <?=$data['VEHICLE_REG']?></td>
+                    </tr>
+                </table>
+                <?php } ?>
+          <div class="editBtn">
+          <i class="fa fa-edit" onclick="openEditPopup()">Edit</i>
+          </div>
+        </div>
+           
+        </div>
+
+        <div id="more-details" class="maindiv">
+            <h3><u> MORE DETAILS</u></h3>
+            <div id="latest-form">
+                <h4>Latest Form</h4>
+                <?php foreach($formArray as $data) { ?>
+                    <table>
+                        <tr>
+                            <td>Name :</td>
+                            <td> <?=$data['NAME']?></td>
+                            <td>Phone number :</td>
+                            <td> <?=$data['PHN']?></td>
+                        </tr>
+                        <tr>
+                            <td>Vehicle Type :</td>
+                            <td> <?=$data['VEHICLE_TYPE']?></td>
+                            <td>Vehicle Category :</td>
+                            <td>  <?=$data['VEHICLE_CAT']?></td>
+                        </tr>
+                        <tr>
+                            <td> Engine CC :</td>
+                            <td> <?=$data['ENGINE_CC']?></td>
+                            <td> Vehicle Registration no. :</td>
+                            <td> <?=$data['VEHICLE_REG']?></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Last Renew Date</td>
+                            <td colspan="2"><?=$data['RENEW_DATE']?></td>
+                        </tr>
+                    </table>
+                <?php } ?>
+            </div>
+        </div>
+    </main> 
      <!-- foot of the website -->
      <footer>
  
