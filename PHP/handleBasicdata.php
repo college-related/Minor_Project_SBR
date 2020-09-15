7a3123dec83a3d9a6843b63c6890a8008eb4fbcd
Salipa-Gurung <salipagurung@gmail.com>
2020-09-15 05:17:41 -0700

    database column name updated

--- PHP/handleBasicdata.php
+++ PHP/handleBasicdata.php
@@ -13,21 +13,21 @@ if( isset($_POST['savebtn'])){
    $vReg=$_POST['regNo'];
    $engineCC=$_POST['ECC']; 
 
-    $sql_phn_check = "SELECT * FROM basic_data WHERE PHN = '$phn';";
+    $sql_phn_check = "SELECT * FROM basic_data WHERE PHN1 = '$phn';";
     $Phn = mysqli_query($connect,$sql_phn_check);
 
     if(mysqli_num_rows($Phn) > 0 ){
-      $sql = "UPDATE basic_data SET VEHICLE_TYPE = '$vType', VEHICLE_CAT = '$vCat', VEHICLE_REG = '$vReg', ENGINE_CC = '$engineCC' WHERE PHN = '$phn' && NAME = '$name';";
+      $sql = "UPDATE basic_data SET VEHICLE_TYPE1 = '$vType', VEHICLE_CAT1 = '$vCat', VEHICLE_REG1 = '$vReg', ENGINE_CC1 = '$engineCC' WHERE PHN1 = '$phn' && NAME1 = '$name';";
 
       $query = mysqli_query($connect,$sql);
 
       if(mysqli_affected_rows($connect)){
-          header("location: ../PAGES/profile.php?Logged");
+          header("location: ../PAGES/profile.php?Logged1");
       }else{
         header("location: ../PAGES/profile.php?Logged&Err");
       }
     }else{
-        $sql = "INSERT INTO basic_data(PHN, NAME, VEHICLE_TYPE, VEHICLE_CAT, VEHICLE_REG, ENGINE_CC) VALUES ('$phn', '$name', '$vType', '$vCat', '$vReg', '$engineCC')";
+        $sql = "INSERT INTO basic_data(PHN1, NAME1, VEHICLE_TYPE1, VEHICLE_CAT1, VEHICLE_REG1, ENGINE_CC1) VALUES ('$phn', '$name', '$vType', '$vCat', '$vReg', '$engineCC')";
         
         $query = mysqli_query($connect, $sql);
 
