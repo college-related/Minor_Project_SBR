f23f05807cbb2ac980d9d83d3cbfcc53da3002b2
Salipa-Gurung <salipagurung@gmail.com>
2020-09-28 01:13:44 -0700

    Activation code added

--- PHP/handleUser.php
+++ PHP/handleUser.php
@@ -8,6 +8,7 @@ if(isset($_POST['signup'])){
     $password =  $_POST['Password'];
     $confirmpassword =  $_POST['Repass'];
     $phonenumber =  $_POST['Phn'];
+    $activateCode = md5(rand());
 
     if($password == $confirmpassword){
         require_once "./connection.php";
@@ -24,13 +25,14 @@ if(isset($_POST['signup'])){
             session_start();
             $_SESSION['Uname'] = $username;
             $_SESSION['phone'] = $phonenumber;
+            $_SESSION['Email'] = $email;
 
-            $sql = "INSERT INTO users(USERNAME, EMAIL, PASSWORD, PHN) VALUES('$username', '$email', '$password', '$phonenumber')";
+            $sql = "INSERT INTO users(USERNAME, EMAIL, PASSWORD, PHN, ACTIVATE_CODE, EMAIL_STATUS) VALUES('$username', '$email', '$password', '$phonenumber', '$activateCode', 'not verified')";
 
             mysqli_query($connect, $sql);
 
             if(mysqli_affected_rows($connect)){
-                header("location: ../PAGES/profile.php?Logged&signed");
+                header("location: ./emailVerification.php");
             }else{
                 header("location: ../Landing.php");
             }
