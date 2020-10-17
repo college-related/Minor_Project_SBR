f223cba9f6e879754d1e4793db612a462ee22ce1
Salipa-Gurung <salipagurung@gmail.com>
2020-10-17 06:32:34 -0700

    new error popup implementation

---
+++ repeated_section/error.php
@@ -0,0 +1,17 @@
+<!-- error popup -->
+
+<div class="error-popup">
+    <!-- icon of the error -->
+    <?php echo $icon; ?>
+        <div id="error-text">
+            <!-- title of the error -->
+            <b>
+                <?php echo $title; ?>
+            </b>
+            <!-- message of the error -->
+            <br>
+                <?php echo $mssg;?>
+            </div>
+            <!-- cross mark to close the error popup -->
+            <span id="errorCloseMark" onclick="closeErrorPopUp()">&times;</span>
+        </div>
