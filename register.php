6fb75fbbb3b17c00c4a76804bade9e0503333230
Salipa-Gurung <salipagurung@gmail.com>
2020-10-17 06:33:29 -0700

    moved the error code from landing.php

--- register.php
+++ register.php
@@ -1,11 +1,107 @@
+<?php
+
+    if(isset($_GET['error'])){
+        $error = $_GET['error'];
+        $title = "Error";
+        
+            if($error == 'IllegalWay'){
+                $icon = "<i class='far fa-angry fa-2x'></i>";
+                $mssg = "Please ! Enter through proper way.";                
+            }else if($error == 'NotInserted'){
+                $icon = "<i class='far fa-grin-beam-sweat fa-2x'></i>";
+                $mssg = "Some error occured. Please retry.";
+            }else if($error == 'NotUser'){
+                $icon = "<i class='fas fa-frown fa-2x'></i>";
+                $mssg = "No such user found.";
+            }else if($error == 'SendMailError'){
+                $icon = "<i class='far fa-sad-cry fa-2x'></i>";
+                $mssg = "Some technical issue detected. Please report.";
+            }else if($error == 'WrongLink'){
+                $icon = "<i class='far fa-dizzy fa-2x'></i>";
+                $mssg = "The link must have been expired.";
+            }else if($error == 'NotActivationCode'){
+                $icon = "<i class='far fa-meh fa-2x'></i>";
+                $mssg = "No such activation code found.";
+            }
+    }
+
+    if(isset($_GET['inputError'])){
+        $inputError = $_GET['inputError'];
+    }
+
+    if(isset($_GET['mssg'])){
+        $successMssg = $_GET['mssg'];
+        $title = "Hurray!";
+
+        if($successMssg == 'CheckEmail'){
+            $icon = "<i class='far fa-grin-beam fa-2x'></i>";
+            $mssg = "Please check your email for verification";
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+
+    <link rel="stylesheet" href="./CSS/error.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <style>
+        <?php
+            if(isset($_GET['error'])){
+                echo ".error-popup{
+                    background-color:rgba(226,154,154,0.116);
+                    color:rgb(185,14,14);
+                    animation: animateFromTop 1s ease-in-out;
+                    top: 50px;
+                    left: 50%;
+                    transform: translateX(-50%);
+                }";
+            }else if(isset($_GET['mssg'])){
+                echo ".error-popup{
+                    background-color:#DFF2BF;
+                    color:#4F8A10;
+                    animation: animateFromBottom 1s ease-in-out;
+                    bottom: 20px;
+                    right: 30%;
+                    transform: translateX(30%);
+                }";
+            }
+
+        ?>
+
+        <?php
+            if(!isset($_GET['error']) && !isset($_GET['mssg'])){
+                echo ".error-popup{
+                    display:none;
+                }";
+            }
+
+        ?>
+
+        <?php
+            if($inputError == 'AlreadyUserEmail'){
+                echo "input[type='email']{
+                        border:1px solid red;
+                }";
+            }else if($inputError == 'PasswordNotSame'){
+                echo "input[type='password']{
+                        border:1px solid red;
+                }";
+            }else if($inputError == 'AlreadyUserPhone'){
+                echo "input[type='number']{
+                        border:1px solid red;
+                }";
+            }
+        ?>
+    </style>
 </head>
 <body>
-    
+    <?php include './repeated_section/error.php'; ?>
 </body>
 </html>
