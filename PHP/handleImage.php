23829a85c5207c0f3b1074870f4b7c7affc6c674
Alson33 <garbuja12alson@gmail.com>
2020-12-08 17:05:35 +0545

    security in image data

--- PHP/handleImage.php
+++ PHP/handleImage.php
@@ -1,10 +1,18 @@
 <?php
 
+function encryptData($data, $key, $str){
+    $encryption_key = base64_decode($key);
+    $ivlength = substr(md5($str."users"),1, 16);
+    $encryptedData = openssl_encrypt($data, "aes-256-cbc", $encryption_key, 0, $ivlength);
+
+    return base64_encode($encryptedData.'::'.$ivlength);
+}
+
     if(isset($_FILES['img']['name'])){
         require('./connection.php');
 
         session_start();
-        $phn = $_SESSION['phone'];
+        $uId = $_SESSION['uId'];
 
         $name = $_FILES["img"]["name"];
         $target_dir = "../ASSETS/upload/";
@@ -16,9 +24,14 @@
 
         if(in_array($imageFileType, $extension_arr)){
 
+            $str = "/6G6F;WvK7;s{au/6G6F;WvK7;s{au";
+            $key = md5($str);
+
+            $name = encryptData($data, $key, $str);
+
             mysqli_query(
                 $connect,
-                "INSERT INTO images VALUES('$phn', '$name');"
+                "INSERT INTO images VALUES('$uId', '$name');"
             );
 
             if(mysqli_affected_rows($connect) > 0){
