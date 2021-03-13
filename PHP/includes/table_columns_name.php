393b7bde34edb34e458c64857204b2a702dfb603
Alson33 <garbuja12alson@gmial.com>
2021-03-13 19:29:54 +0545

    add table columns name file to inlcude

--- PHP/includes/table_columns_name.php
+++ PHP/includes/table_columns_name.php
@@ -11,10 +11,29 @@ $phoneNumber_column = "phone_number";
 $citizenship_column = "citizenship_number";
 $activation_column = "activation_code";
 $emailVerification_column = "email_verified";
+$image_column = "profile_image_name";
 
-// vehicles_data table
+// vehicles_data table / forms_data table
 $vehicleRegistration_column = "vehicle_registration_number";
 $vehicleType_column = "vehicle_type";
 $vehicleCategory_column = "vehicle_category";
 $engineCC_column = "engine_cc";
 $pp_column = "public_private";
+
+// froms_data table
+$formID_column = "fId";
+$formFillerId_column = "form_filler_uId";
+$insurance_column = "insurance_slip";
+$renewDate_column = "last_renew_date";
+$formFillerVehicleReg_column = "form_filler_vehicle_registration_number";
+$formFillerName_column = "form_filled_by";
+$formFillerPhn_column = "form_filler_phone_number";
+
+
+// tax_details table
+$fillerId_column = "filler_uId";
+$taxAmount_column = "tax_amount";
+$fineAmount_column = "fine_amount";
+$finePercentage_column = "fine_percentage";
+$paidIn_column = "paid_in";
+$totalAmount_column = "total_amount";
