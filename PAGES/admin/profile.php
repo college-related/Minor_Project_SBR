b6e397dc9a639a4b521f726d32f01ec281d9fb8b
Alson33 <garbuja12alson@gmial.com>
2021-06-30 23:54:41 +0545

    add admin pages draft

---
+++ PAGES/admin/profile.php
@@ -0,0 +1,21 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Profile</title>
+
+    <link rel="stylesheet" href="../../CSS/new-css/style.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+    <?php include('../../repeated_section/header.php') ?>
+
+    <main>
+        Profile
+    </main>
+</body>
+</html>
