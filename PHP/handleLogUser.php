235792ba2464cfc22c7f46e13622b1d3b548847f
Alson33 <garbuja12alson@gmial.com>
2021-06-30 23:54:58 +0545

    add redirect to admin sides

--- PHP/handleLogUser.php
+++ PHP/handleLogUser.php
@@ -13,13 +13,13 @@ if(isset($_POST['login'])){
     $str = "/6G6F;WvK7;s{au/6G6F;WvK7;s{au";
     $key = md5($str);
 
-    $username_sql = "SELECT $password_column, $emailVerification_column, uId FROM users WHERE $username_column = '$user';";
+    $username_sql = "SELECT $password_column, $emailVerification_column, $role_column, uId FROM users WHERE $username_column = '$user';";
     $result = mysqli_query($connect, $username_sql);
     
     // * checking if the user with the given name is a valid user or not
     if(mysqli_num_rows($result) > 0){
         $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-            
+
         foreach($row as $data){
             // * checking if the email is verified or not
             if($data[$emailVerification_column] == "not verified"){
@@ -32,15 +32,21 @@ if(isset($_POST['login'])){
                 session_start();
                 $_SESSION['uId'] = $data['uId'];
 
-                header("location: ../PAGES/profile.php?Logged");
-                die();
+                if($data[$role_column] == 'admin'){
+                    $_SESSION['role'] = 'admin';
+                    header("location: ../PAGES/admin/profile.php");
+                    die();
+                }else{
+                    header("location: ../PAGES/profile.php?Logged");
+                    die();
+                }
             }else{
                 header("location: ../index.php?inputError=WrongPass#loginForm");
             }
         }
     }else{
         $user = encryptData($user, $key, $str);    
-        $email_sql = "SELECT $password_column, $emailVerification_column, uId FROM users WHERE $email_column = '$user';";
+        $email_sql = "SELECT $password_column, $emailVerification_column, $role_column, uId FROM users WHERE $email_column = '$user';";
         $result = mysqli_query($connect, $email_sql);
         
         // * checking if the user with the given email is a valid user
@@ -58,7 +64,14 @@ if(isset($_POST['login'])){
 
             // * checking if the password is correct or not
             if(password_verify($Password,$row[$password_column])){
-                header("location: ../PAGES/profile.php?Logged");
+                if($row[$role_column] == 'admin'){
+                    $_SESSION['role'] = 'admin';
+                    header("location: ../PAGES/admin/profile.php");
+                    die();
+                }else{
+                    header("location: ../PAGES/profile.php?Logged");
+                    die();
+                }
             }else{
                 header("location: ../index.php?inputError=WrongNameORPass#loginForm");
             }
