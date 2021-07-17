c8e156ff8b9ef796e4a36f44c7df53e4b5682d09
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2021-07-17 16:54:55 +0545

    add forget password page

--- PAGES/forgetPassword.php
+++ PAGES/forgetPassword.php
@@ -4,22 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forgot Password</title>
+    <link rel="stylesheet" href="../CSS/new-css/color.css">
+    <link rel="stylesheet" href="../CSS/new-css/style.css">
     <link rel="stylesheet" href="../CSS/forgot_reset.css">
 </head>
 <body>
-<main>
-        <div class="details">
-            <h2>Forgot Your Password?</h2>
-            <h5 id="msg">Don't worry we got you.</h5>
-            <span>Enter the email you want to reset the password</span>
-            <form action="../PHP/passwordResetLink.php" method="post">
-                <input type="email" name="email" id="Email" placeholder="Enter the email"/><br><br>
-                <input type="submit" value="Send reset link" name="resetLink">
-            </form>
-            <div class="account">Already have an account? <a href="../index.php#loginForm">Log in</a></div>
+    <main  class="container form" >
+        <div class="flex main-div">
+            <div class="detail">
+                    <h2 class="form-title web-title">Forgot Your Password?</h2>
+                    <span >We got you.<br>Enter the email you want to reset the password</span>
+                    <div class="form-body">
+                        <form action="../PHP/passwordResetLink.php" method="post" >
+                            <input type="email" name="email" id="Email" placeholder="Your Email" class="form-field form-group"/><br><br>
+                            <input type="submit" value="Send Reset Link" name="resetLink" class="btn btn-accent  form-group">
+                        </form>
+                    </div>
+                    <div class="form-footer">
+                        <span>Got Password?</span>
+                        <a href="../index.php#loginForm" class="link-primary">Log in</a>
+                    </div>
+            </div>
+            <div class="forgot-password-image">
+                <h2 class="form-title mobile-title">Forgot Your Password?</h2>
+                <img src="../ASSETS/IMAGES/undraw/forgot-password.png" alt="forgot-password" class="feature-img">
+            </div>
         </div>
-        <div class="illustration"></div>
-        
     </main>
+    <script src="../JS/new-js/mobile-menu.js"></script>
+    <script src="../JS/new-js/theme.js"></script>
 </body>
 </html>
