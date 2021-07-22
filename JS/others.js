3a337ad897de746a06f24937fe16dedb0da20052
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2021-07-22 18:34:12 +0545

    bug

--- JS/others.js
+++ JS/others.js
@@ -3,7 +3,7 @@ var autoBtn = document.getElementById("autofill");
 function reveal() {
     if (show.style.display === "none") {
         show.style.display = "block";
-        // autoBtn.setAttribute("disabled", "true");
+        autoBtn.setAttribute("disabled", "true");
     } else {
         show.style.display = "none";       
         // autoBtn.removeAttribute("disabled");
