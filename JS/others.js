2837164ca5b7467432754154a580c5cf5fc020e9
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2021-07-22 18:30:26 +0545

    add js for hidden div

--- JS/others.js
+++ JS/others.js
@@ -1,21 +1,11 @@
-var others1 = document.getElementsByClassName("others-info")[0];
-var others2 = document.getElementsByClassName("others-info")[1];
-var check = document.getElementById("check");
+var show = document.getElementById("display");
 var autoBtn = document.getElementById("autofill");
-var resetBtn = document.getElementById("resetBtn");
-
-// function to reveal the others section
 function reveal() {
-    // check if the checkbox is checked or not
-    if(check.checked == true){
-        others1.style.display = "block";
-        others2.style.display = "block";
+    if (show.style.display === "none") {
+        show.style.display = "block";
         autoBtn.setAttribute("disabled", "true");
-        resetBtn.removeAttribute("disabled");
-    }else{
-        others2.style.display = "none";
-        others1.style.display = "none";
-        autoBtn.removeAttribute("disabled");
-        resetBtn.setAttribute("disabled", "true");
+    } else {
+        show.style.display = "none";       
+        // autoBtn.removeAttribute("disabled");
     }
-}
+  }
