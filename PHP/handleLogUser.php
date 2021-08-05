df037fc5b4a03dbf903d779740e90bf318e7868d
Alson33 <garbuja12alson@gmial.com>
2021-08-05 21:49:32 +0545

    change admin profile page name

--- PHP/handleLogUser.php
+++ PHP/handleLogUser.php
@@ -34,7 +34,7 @@ if(isset($_POST['login'])){
 
                 if($data[$role_column] == 'admin'){
                     $_SESSION['role'] = 'admin';
-                    header("location: ../PAGES/admin/profile.php");
+                    header("location: ../PAGES/admin/admin-profile.php");
                     die();
                 }else{
                     header("location: ../PAGES/profile.php");
@@ -66,7 +66,7 @@ if(isset($_POST['login'])){
                 
                 if($row[$role_column] == 'admin'){
                     $_SESSION['role'] = 'admin';
-                    header("location: ../PAGES/admin/profile.php");
+                    header("location: ../PAGES/admin/admin-profile.php");
                     die();
                 }else{
                     header("location: ../PAGES/profile.php?Logged");
--- repeated_section/navMenu.php
+++ repeated_section/navMenu.php
@@ -1,7 +1,7 @@
 
 <?php if(isset($_SESSION['uId']) && isset($_SESSION['role']) && $_SESSION['role'] == 'admin') { ?>
     <li><a href="http://localhost/MINOR_PROJECT_SBR/PAGES/admin/details.php" class='<?php if($pageName == "details.php") { echo "active-link"; } ?>'><?=$lang['header']['detail-text']?></a></li>
-    <li><a href="http://localhost/MINOR_PROJECT_SBR/PAGES/admin/profile.php" class='<?php if($pageName == "profile.php") { echo "active-link"; } ?>'><?=$lang['header']['profile-text']?></a></li>
+    <li><a href="http://localhost/MINOR_PROJECT_SBR/PAGES/admin/admin-profile.php" class='<?php if($pageName == "admin-profile.php") { echo "active-link"; } ?>'><?=$lang['header']['profile-text']?></a></li>
 <?php }else {?>
     <li><a href="http://localhost/MINOR_PROJECT_SBR/PAGES/taxCalculator.php" class='<?php if($pageName == "taxCalculator.php") { echo "active-link"; } ?>'><?=$lang['header']['tax-text']?></a></li>
     <li><a href="http://localhost/MINOR_PROJECT_SBR/PAGES/information.php" class='<?php if($pageName == "information.php") { echo "active-link"; } ?>'><?=$lang['header']['info-text']?></a></li>
